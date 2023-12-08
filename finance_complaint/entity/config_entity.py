67219f0b5b3e7291b266c54cecf051f5c6511f65
Abhishek Jana <iiserabhishek@gmail.com>
2023-12-09 04:19:08 +0530

    data ingestion complete

--- finance_complaint/entity/config_entity.py
+++ finance_complaint/entity/config_entity.py
@@ -1,6 +1,6 @@
 import os,sys
 from dataclasses import dataclass
-from finance_complaint.metadata_entity import DataIngestionMetadata
+from finance_complaint.entity.metadata_entity import DataIngestionMetadata
 from datetime import datetime
 from finance_complaint.exception import FinanceException
 from finance_complaint.constant import TIMESTAMP
@@ -12,7 +12,7 @@ DATA_INGESTION_FILE_NAME = "finance_complaint"
 DATA_INGESTION_FEATURE_STORE_DIR = "feature_store"
 DATA_INGESTION_FAILED_DIR = "failed_downloaded_files"
 DATA_INGESTION_METADATA_FILE_NAME = "meta_info.yaml"
-DATA_INGESTION_MIN_START_DATE = "2023-01-01"
+DATA_INGESTION_MIN_START_DATE = "2023-05-01"
 DATA_INGESTION_DATA_SOURCE_URL = f"https://www.consumerfinance.gov/data-research/consumer-complaints/search/api/v1/" \
                       f"?date_received_max=<todate>&date_received_min=<fromdate>" \
                       f"&field=all&format=json"
--- finance_complaint/entity/metadata_entity.py
+++ finance_complaint/entity/metadata_entity.py
@@ -1,3 +1,4 @@
+import os,sys
 from finance_complaint.exception import FinanceException
 from finance_complaint.logger import logging
 from finance_complaint.utils import read_yaml_file, write_yaml_file
@@ -14,7 +15,7 @@ class DataIngestionMetadata:
 
     @property
     def is_metadata_file_present(self) -> bool:
-        return os.path.exists(self.meatadata_file_path)
+        return os.path.exists(self.metadata_file_path)
 
     def write_metadata_info(self, from_date: str, to_date: str, data_file_path: str):
         try:
@@ -34,7 +35,7 @@ class DataIngestionMetadata:
                 raise Exception("No Metadata file available")
             metadata = read_yaml_file(self.metadata_file_path)
             metadata_info = DataIngestionMetadataInfo(**(metadata))
-            logger.info(meatadata)
-            return meatadata_info
+            logging.info(metadata)
+            return metadata_info
         except Exception as e:
             raise FinanceException(e,sys)
